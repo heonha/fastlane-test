3be4648b7656aa8996f131037ae68339883e0516
Heonjin Ha <weizen511@gmail.com>
2023-03-06 19:04:20 +0900

    Initial Commit

---
+++ fastlane-test/fastlane_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  fastlane_testApp.swift
+//  fastlane-test
+//
+//  Created by Heonjin Ha on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct fastlane_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
